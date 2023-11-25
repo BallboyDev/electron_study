01e6610a4f81d67f51321a8dfba69adc390e2d89
ballboyDev <yswgood0329@gmail.com>
2023-11-25 18:25:22 +0900

    electron study init

---
+++ electron-base/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, clipboard } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            // Dom 로딩 후 실행
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+// app : event lifecycle 관리
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+/** Package and distribute application */
+/**
+ * npm install --save-dev @electron-forge/cli
+ * npx electron-forge import
+ * 
+ * npm run make
+ */
